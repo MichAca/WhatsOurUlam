b1a1d2ac88df4e4a0184c115fad3e36e41bf7545
Michelle Aca <michelleaca@Mich.local>
2019-04-19 04:12:22 +0800

    :memo: added code for  app functionality

--- app/src/main/java/com/bordscode/whatsourulam/MainActivity.kt
+++ app/src/main/java/com/bordscode/whatsourulam/MainActivity.kt
@@ -2,11 +2,31 @@ package com.bordscode.whatsourulam
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
+
+    private val foodList = arrayListOf("Adobo", "Sinigang", "Tinola", "Nilaga", "Siarsiado" )
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        chooseFoodButton.setOnClickListener {
+
+            var random = Random()
+            var randomFood = random.nextInt(foodList.count())
+            selectedFoodTextView.text = foodList[randomFood]
+
+        }
+
+        addFoodButton.setOnClickListener {
+
+            val newFood = addFoodTextView.text.toString()
+            foodList.add(newFood)
+            addFoodTextView.text.clear()
+            println(foodList)
+        }
     }
 }
